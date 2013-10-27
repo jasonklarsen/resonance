156b1380219bc5a9ec5727f24ddb3540595e5cd7
Jason Larsen <jasonklarsen@gmail.com>
2013-10-27 12:24:14 -0400

    Refactored the server start

--- server/finagle/src/main/scala/io/larsen/resonance/Server.scala
+++ server/finagle/src/main/scala/io/larsen/resonance/Server.scala
@@ -1,6 +1,8 @@
 package io.larsen.resonance.finagle
 
-import com.twitter.finagle.{Http, Service, SimpleFilter}
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.finagle.http.{Http, Request, Response, RichHttp}
+import com.twitter.finagle.builder.{ServerBuilder}
 import com.twitter.util.{Await, Future}
 import java.net.InetSocketAddress
 import org.jboss.netty.handler.codec.http._
@@ -12,22 +14,38 @@ import java.lang.System.{getProperty, setProperty}
 object Server extends App {
   val resPort = getProperty("resPort")
   val service = (new HandleExceptions).andThen(new SimpleResponse)
-  val server = Http.serve("localhost:" + resPort, service)
+//  val server = Http.serve("localhost:" + resPort, service)
+val server  = ServerBuilder()
+      .codec(RichHttp[Request](Http()))
+      .bindTo(new InetSocketAddress(resPort.toInt))
+      .name("resonance-server-finagle")
+      .build(service)
   println("**Starting the finagle resonance server**")
-  Await.ready(server)
+//  Await.ready(server)
 }
 
-class SimpleResponse extends Service[HttpRequest, HttpResponse] {
-	def apply(req: HttpRequest): Future[HttpResponse] = {
+class SimpleResponse extends Service[Request, Response] {
+	def apply(req: Request): Future[Response] = {
     println("**Building response**")
-		val response = new DefaultHttpResponse(req.getProtocolVersion, HttpResponseStatus.OK)
-		val sysTime = io.larsen.resonance.Core.sysTime
 
-		response.setHeader("Access-Control-Allow-Origin", webAddress)
-		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With")
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST")
-		response.setContent(copiedBuffer("{\"sysTime\":\"" + sysTime + "\"}", UTF_8))
-		Future.value(response)
+    Future.value {
+      val sysTime = io.larsen.resonance.Core.sysTime
+      val response = Response()
+      response.setContentTypeJson
+      response.setHeader("Access-Control-Allow-Origin", webAddress)
+      response.setHeader("Access-Control-Allow-Headers", "X-Requested-With")
+      response.setHeader("Access-Control-Allow-Methods", "GET, POST")
+      response.content = copiedBuffer("{\"sysTime\":\"" + sysTime + "\"}", UTF_8)
+      response
+    }
+
+		// val response = new DefaultHttpResponse(req.getProtocolVersion, HttpResponseStatus.OK)
+
+		// response.setHeader("Access-Control-Allow-Origin", webAddress)
+		// response.setHeader("Access-Control-Allow-Headers", "X-Requested-With")
+		// response.setHeader("Access-Control-Allow-Methods", "GET, POST")
+		// response.setContent(copiedBuffer("{\"sysTime\":\"" + sysTime + "\"}", UTF_8))
+		// Future.value(response)
 	}
 
   private def webAddress() = {
@@ -38,8 +56,8 @@ class SimpleResponse extends Service[HttpRequest, HttpResponse] {
 }
 
 
-class HandleExceptions extends SimpleFilter[HttpRequest, HttpResponse] {
-  def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]) = {
+class HandleExceptions extends SimpleFilter[Request, Response] {
+  def apply(request: Request, service: Service[Request, Response]) = {
     service(request) handle { case error =>
       val statusCode = error match {
         case _: IllegalArgumentException =>
@@ -48,9 +66,8 @@ class HandleExceptions extends SimpleFilter[HttpRequest, HttpResponse] {
           HttpResponseStatus.INTERNAL_SERVER_ERROR
         }
 
-      val errorResponse = new DefaultHttpResponse(request.getProtocolVersion, statusCode)
-      errorResponse.setContent(copiedBuffer(error.getStackTraceString, UTF_8))
-
+      val errorResponse = Response(request.getProtocolVersion, statusCode)
+      errorResponse.content = copiedBuffer(error.getStackTraceString, UTF_8)
       errorResponse
     }
   }
